7387f02e0ca4e0fe51d0d04a335768978abbb515
Ameen Soleimani <ameen.sol@gmail.com>
2018-12-06 22:23:25 +0800

    signature invalid

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,27 @@
 const EC = artifacts.require("./ECTools.sol");
-const LC = artifacts.require("./ChannelManager.sol");
+const CM = artifacts.require("./ChannelManager.sol");
 const HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(EC);
 
-  let tokenAddress = "0x0"; // change to BOOTY address for mainnet
+  const data = require('../data.json')
+
+  let tokenAddress // TODO change to BOOTY address for mainnet
 
   if (network !== "mainnet" && network !== "rinkeby") {
     const supply = web3.utils.toBN(web3.utils.toWei("696969", "ether"));
     await deployer.deploy(
       HumanStandardToken,
-      supply,
-      "Test Token",
-      "18",
-      "TST"
+      data.token.supply,
+      data.token.name,
+      data.token.decimals,
+      data.token.symbol
     );
     const hst = await HumanStandardToken.deployed();
     tokenAddress = hst.address;
   }
 
-  await deployer.link(EC, LC);
-  await deployer.deploy(LC, accounts[0], 10000, tokenAddress);
+  await deployer.link(EC, CM);
+  await deployer.deploy(CM, accounts[0], data.channelManager.challengePeriod, tokenAddress);
 };
--- test/channelManager.js
+++ test/channelManager.js
@@ -1,28 +1,26 @@
-"use strict";
 const should = require("chai")
-// const Connext = require("../client/dist/Utils.js");
 const HttpProvider = require("ethjs-provider-http")
 const EthRPC = require("ethjs-rpc")
-const config = require("./config.json")
-const Utils = require("./helpers/utils");
+const { StateGenerator } = require('@spankchain/connext-client/dist/StateGenerator')
+const { makeSuccinctChannel } = require('@spankchain/connext-client/dist/testing/index')
+const { Utils } = require('@spankchain/connext-client/dist/Utils')
+const { addSigToChannelState } = require('@spankchain/connext-client/dist/types')
 const privKeys = require("./privKeys.json")
-const EC = artifacts.require("./ECTools.sol")
-// const Ledger = artifacts.require("./ChannelManager.sol")
-// const Token = artifacts.require("./lib/HumanStandardToken.sol")
+const CM = artifacts.require("./ChannelManager.sol")
+const HST = artifacts.require("./HumanStandardToken.sol")
 
-const Connext = require('./../client/dist/Connext');
+const data = require('../data.json')
 
-console.log(Connext)
-
-
-should
-  .use(require("chai-as-promised"))
-  .should()
+should.use(require("chai-as-promised")).should()
 
 const ethRPC = new EthRPC(new HttpProvider('http://localhost:8545'))
-const SolRevert = "VM Exception while processing transaction: revert"
 const emptyRootHash =
   "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+const SolRevert = 'VM Exception while processing transaction: revert'
+
+const secondsFromNow = (seconds) => seconds + Math.floor(new Date().getTime() / 1000)
+const minutesFromNow = (minutes) => secondsFromNow(minutes * 60)
 
 async function snapshot() {
     return new Promise((accept, reject) => {
@@ -53,21 +51,10 @@ async function moveForwardSecs(secs) {
     jsonrpc:'2.0', method: `evm_increaseTime`,
     params: [secs],
     id: 0
-  }, (err)=> {`error increasing time`});
-  const start = Date.now();
-  // TODO why do we do these empty loops?
-  while (Date.now() < start + 300) {}
-  await ethRPC.sendAsync({method: `evm_mine`}, (err)=> {});
-  while (Date.now() < start + 300) {}
+  }, (err)=> {`error increasing time`})
+  const start = Date.now()
+  await ethRPC.sendAsync({method: `evm_mine`}, (err)=> {})
   return true
-}
-
-async function generateThreadProof(threadHashToProve, threadInitStates) {
-  return await Connext.Utils.generateThreadProof(threadHashToProve, threadInitStates)
-}
-
-async function generateThreadRootHash(threadInitStates){
-  return await Connext.Utils.generateThreadRootHash(threadInitStates)
 }
 
 function getEventParams(tx, event) {
@@ -81,20 +68,86 @@ function getEventParams(tx, event) {
   return false
 }
 
-let connext
 
+async function getSignedState(state, account, isUser=true) {
+  console.log(state)
+  const hash = await web3.utils.soliditySha3(
+    cm.address,
+    {type: 'address[2]', value: [state.user, state.recipient]},
+    {type: 'uint256[2]', value: state.weiBalances},
+    {type: 'uint256[2]', value: state.tokenBalances},
+    {type: 'uint256[4]', value: state.pendingWeiUpdates},
+    {type: 'uint256[4]', value: state.pendingTokenUpdates},
+    {type: 'uint256[2]', value: state.txCount},
+    {type: 'bytes32', value: state.threadRoot},
+    state.threadCount,
+    state.timeout
+  )
+  const { signature } = await web3.eth.accounts.sign(hash, account.pk)
+  return {
+    ...state,
+    sigUser: isUser ? signature : state.sigUser || '',
+    sigHub: !isUser ? signature : state.sigHub || ''
+  }
+}
 
+async function userAuthorizedUpdate(state, account, wei=0) {
+  await cm.userAuthorizedUpdate(
+    state.recipient,
+    state.weiBalances,
+    state.tokenBalances,
+    state.pendingWeiUpdates,
+    state.pendingTokenUpdates,
+    state.txCount,
+    state.threadRoot,
+    state.threadCount,
+    state.timeout,
+    state.sigHub,
+    {from: account.address, value: wei}
+  )
+}
+
+let sg = new StateGenerator()
+let utils = new Utils()
+
+let cm, token, hub, performer, viewer, state
 
 contract("ChannelManager", accounts => {
   let snapshotId
 
-  connext = createConnext(web3, accounts[0])
-
   before('deploy contracts', async () => {
+    cm = await CM.deployed()
+    token = await HST.deployed()
+
+    hub = {
+      address: accounts[0],
+      pk: privKeys[0]
+    }
+    performer = {
+      address: accounts[1],
+      pk: privKeys[1]
+    }
+    viewer = {
+      address: accounts[2],
+      pk: privKeys[2]
+    }
   })
 
   beforeEach(async () => {
     snapshotId = await snapshot()
+
+    state = {
+      user: performer.address,
+      recipient: performer.address,
+      weiBalances: [0, 0],
+      tokenBalances: [0, 0],
+      pendingWeiUpdates: [0, 0, 0, 0],
+      pendingTokenUpdates: [0, 0, 0, 0],
+      txCount: [1, 1],
+      threadRoot: emptyRootHash,
+      threadCount: 0,
+      timeout: 0
+    }
   })
 
   afterEach(async () => {
@@ -103,6 +156,138 @@ contract("ChannelManager", accounts => {
 
   describe('contract deployment', () => {
     it("verify init parameters", async () => {
+      const hubAddress = await cm.hub.call()
+      assert.equal(hubAddress, hub.address)
+      const challengePeriod = await cm.challengePeriod.call()
+      assert.equal(data.channelManager.challengePeriod, challengePeriod)
+      const approvedToken = await cm.approvedToken.call()
+      assert.equal(token.address, approvedToken)
     })
   })
+
+  describe('reserve management', () => {
+    it("accept ETH - getHubReserveWei", async () => {
+      const weiAmount = 1
+      await web3.eth.sendTransaction({ from: hubAddress, to: cm.address, value: weiAmount })
+      const reserveWei = await cm.getHubReserveWei()
+      assert.equal(reserveWei, weiAmount)
+    })
+
+    it("accept tokens - getHubReserveTokens", async () => {
+      const tokenAmount = 1
+      await token.transfer(cm.address, tokenAmount, { from: hubAddress })
+      const reserveTokens = await cm.getHubReserveTokens()
+      assert.equal(reserveTokens, tokenAmount)
+    })
+
+    describe("hubContractWithdraw", () => {
+      it("happy case", async () => {
+        const weiAmount = 1
+        const tokenAmount = 1
+        const hubInitialTokens = await token.balanceOf(hubAddress)
+        await web3.eth.sendTransaction({ from: hubAddress, to: cm.address, value: weiAmount })
+        await token.transfer(cm.address, tokenAmount, { from: hubAddress })
+        await cm.hubContractWithdraw(weiAmount, tokenAmount)
+        const reserveTokens = await cm.getHubReserveTokens()
+        const reserveWei = await cm.getHubReserveWei()
+        assert.equal(reserveWei, 0)
+        assert.equal(reserveTokens, 0)
+
+        const hubFinalTokens = await web3.eth.getBalance(hubAddress)
+        assert.equal(hubInitialTokens, hubInitialTokens)
+      })
+
+      it("fails with insufficient ETH", async () => {
+        const weiAmount = 1
+        const tokenAmount = 1
+        const weiToWithdraw = weiAmount + 1
+        await web3.eth.sendTransaction({ from: hubAddress, to: cm.address, value: weiAmount })
+        await token.transfer(cm.address, tokenAmount, { from: hubAddress })
+        await cm.hubContractWithdraw(weiToWithdraw, tokenAmount).should.be.rejectedWith(
+          `${SolRevert} hubContractWithdraw: Contract wei funds not sufficient to withdraw`
+        )
+      })
+
+      it("fails with insufficient tokens", async () => {
+        const weiAmount = 1
+        const tokenAmount = 1
+        const tokensToWithdraw = tokenAmount + 1
+        await web3.eth.sendTransaction({ from: hubAddress, to: cm.address, value: weiAmount })
+        await token.transfer(cm.address, tokenAmount, { from: hubAddress })
+        await cm.hubContractWithdraw(weiAmount, tokensToWithdraw).should.be.rejectedWith(
+          `${SolRevert} hubContractWithdraw: Contract token funds not sufficient to withdraw`
+        )
+      })
+    })
+  })
+
+  describe("userAuthorizedUpdate", () => {
+    it.only('deposit wei', async () => {
+      // TODO use proposePendingDeposit
+      // const update = sg.proposePendingDeposit(state, { depositWeiUser: 1 })
+      const update = {
+        ...state,
+        pendingWeiUpdates: [0, 0, 1, 0], // user deposit
+        txCount: [1, 1],
+        timeout: minutesFromNow(5)
+      }
+      const signedUpdate = await getSignedState(update, hub, false)
+      await userAuthorizedUpdate(signedUpdate, viewer, 1)
+      // const channel = await cm.channels.call(viewer.address)
+
+      const oldWaySig = await updateHash(update, hub.pk)
+    })
+
+    async function updateHash(data, privateKey) {
+      const hash = await web3.utils.soliditySha3(
+        cm.address,
+        {type: 'address[2]', value: [data.user, data.recipient]},
+        {type: 'uint256[2]', value: data.weiBalances},
+        {type: 'uint256[2]', value: data.tokenBalances},
+        {type: 'uint256[4]', value: data.pendingWeiUpdates},
+        {type: 'uint256[4]', value: data.pendingTokenUpdates},
+        {type: 'uint256[2]', value: data.txCount},
+        {type: 'bytes32', value: data.threadRoot},
+        data.threadCount,
+        data.timeout
+      )
+      const sig = await web3.eth.accounts.sign(hash, privateKey)
+      return sig.signature
+    }
+
+    // user deposit
+    // init channel state
+    // sign from user
+    // sign from hub
+  })
 })
+
+// https://github.com/ConnextProject/contracts/blob/master/docs/aggregateUpdates.md
+// 1. user deposit
+// 2. hub deposit
+// 3. user withdrawal
+// 4. hub withdrawal
+// 5. user deposit + hub deposit
+// 6. user deposit + hub withdrawal
+// 7. user withdrawal + hub deposit
+// 8. user w + hub w
+// 9. actual exchange scenarios
+//    - performer withdrawal booty -> eth
+//      - also hub withdraws collateral
+//    - user withdrawal booty -> eth
+//      - also hub withdraws collateral
+// 10. recipient is different than user
+//
+// State Transitions:
+// 1. channelBalances (wei / token)
+// 2. totalChannelWei
+// 3. totalChannelToken
+// 4. channel.weiBalances[2]
+// 5. channel.tokenBalances[2]
+// 6. recipient ether balance
+// 7. recipient token balance
+// 8. contract eth/token balance (reserve)
+// 9. txCount
+// 10. threadRoot
+// 11. threadCount
+// 12. event
